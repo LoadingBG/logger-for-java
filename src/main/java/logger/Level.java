544f01db3c6b1057b27973245cc5e7eb8f6a3c03
LoadingBG <51295058+LoadingBG@users.noreply.github.com>
2020-04-21 13:42:09 +0300

    Initial commit

---
+++ src/main/java/logger/Level.java
@@ -0,0 +1,67 @@
+package logger;
+
+/**
+ * Represents the level of severity for logs.
+ */
+enum Level {
+	/**
+	 * Generic text, no color.
+	 */
+	GENERIC, 
+	/**
+	 * Information, blue color.
+	 */
+	INFO, 
+	/**
+	 * Debugging information, green color.
+	 */
+	DEBUG, 
+	/**
+	 * Warnings, yellow colors.
+	 */
+	WARNING, 
+	/**
+	 * Errors, red color.
+	 */
+	ERROR;
+	
+	/**
+	 * ANSI code for blue-colored text.
+	 */
+	private static final String BLUE_TEXT_COLOR = "\u001b[36;1m";
+	/**
+	 * ANSI code for green-colored text.
+	 */
+	private static final String GREEN_TEXT_COLOR = "\u001b[32;1m";
+	/**
+	 * ANSI code for yellow-colored text.
+	 */
+	private static final String YELLOW_TEXT_COLOR = "\u001b[33;1m";
+	/**
+	 * ANSI code for red-colored text.
+	 */
+	private static final String RED_TEXT_COLOR = "\u001b[31;1m";
+	
+	/**
+	 * Gives the specific prefix for the given level.
+	 * 
+	 * @param level The level.
+	 * @param ansi Whether or not to include ANSI text coloring.
+	 * @return The prefix for the level.
+	 */
+	static String getLevelPrefix(Level level, boolean ansi) {
+		switch (level) {
+		case INFO:
+			return (ansi ? BLUE_TEXT_COLOR : "") + "[INFO]: ";
+		case DEBUG:
+			return (ansi ? GREEN_TEXT_COLOR : "") + "[DEBUG]: ";
+		case WARNING:
+			return (ansi ? YELLOW_TEXT_COLOR : "") + "[WARNING]: ";
+		case ERROR:
+			return (ansi ? RED_TEXT_COLOR : "") + "[ERROR]: ";
+		case GENERIC:
+		default:
+			return "";
+		}
+	}
+}
